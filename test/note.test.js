9c2d9612003e9c7bb7018a93e020a8877646a68c
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-19 10:23:42 -0700

    created a test for the note POST route

---
+++ test/note.test.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const request = require('supertest');
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+const app = require('../lib/app');
+const Note = require('../lib/models/Note');
+
+describe('note route tests', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+  it('can POST a new note', () => {
+    return request(app)
+      .post('/api/v1/note')
+      .send({
+        title: 'New Note!',
+        body: 'I am a pretty new note'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          __v: 0,
+          _id: expect.any(String),
+          title: 'New Note!',
+          body: 'I am a pretty new note'
+        });
+      });
+  });
+});
